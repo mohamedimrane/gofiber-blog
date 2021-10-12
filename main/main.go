b243eb2872dc4cff161897218bca3f23e5bdbb28
Med Imrane <imrane@chehabi.ma>
2021-10-12 17:55:17 +0100

    Create a fiber app

--- main/main.go
+++ main/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "github.com/gofiber/fiber/v2"
 
+func main() {
+	var app *fiber.App = fiber.New(fiber.Config{
+		CaseSensitive: true,
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		c.SendString("Hello")
+
+		return nil
+	})
+
+	app.Listen(":8080")
 }
