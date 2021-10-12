d97a04edd106635175a884e574885a6463ac1f36
Med Imrane <imrane@chehabi.ma>
2021-10-12 20:31:44 +0100

    Adds todos to the post handlers

--- handlers/posts.go
+++ handlers/posts.go
@@ -18,6 +18,9 @@ func NewPostsHandler() PostsHandler {
 
 // GetPosts returns to the client all the posts stored in the database
 func (p *PostsHandler) GetPosts(c *fiber.Ctx) error {
+	// TODO: Getting all the posts from database
+	// TODO: Returning the posts to the client as JSON
+
 	c.SendString("Getting posts")
 
 	return nil
@@ -25,6 +28,10 @@ func (p *PostsHandler) GetPosts(c *fiber.Ctx) error {
 
 // GetPost returns to the client a specific post based on its given id in the route
 func (p *PostsHandler) GetPost(c *fiber.Ctx) error {
+	// TODO: Retrive the id from route as an int
+	// TODO: Getting the post from database
+	// TODO: Returning the post to the client as JSON
+
 	id, err := c.ParamsInt("id")
 	if err != nil {
 		return fiber.ErrNotFound
@@ -36,6 +43,11 @@ func (p *PostsHandler) GetPost(c *fiber.Ctx) error {
 
 // AddPost adds a post populated by the data in the request body to the database
 func (p *PostsHandler) CreatePost(c *fiber.Ctx) error {
+	// TODO: Retrieve post data from request body
+	// TODO: Validate request body
+	// TODO: Create post with the given data in the database
+	// TODO: Return to the client the id of the post
+
 	c.SendString("Creating post")
 
 	return nil
@@ -43,6 +55,12 @@ func (p *PostsHandler) CreatePost(c *fiber.Ctx) error {
 
 // AddPost updates a post in the database with the data in the request body and given id in its route
 func (p *PostsHandler) UpdatePost(c *fiber.Ctx) error {
+	// TODO: Retrive the id from route as an int
+	// TODO: Retrieve post data from request body
+	// TODO: Validate request body
+	// TODO: Update post with the given data in the database
+	// TODO: Return to the client the id of the post
+
 	id, err := c.ParamsInt("id")
 	if err != nil {
 		return fiber.ErrNotFound
@@ -54,6 +72,9 @@ func (p *PostsHandler) UpdatePost(c *fiber.Ctx) error {
 
 // AddPost detetes the post with the id given in its route
 func (p *PostsHandler) DeletePost(c *fiber.Ctx) error {
+	// TODO: Retrive the id from route as an int
+	// TODO: Delete the post with the given id
+
 	id, err := c.ParamsInt("id")
 	if err != nil {
 		return fiber.ErrNotFound
