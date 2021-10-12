3f2fb7ad39848971b02587d0bc81c018d7527897
Med Imrane <imrane@chehabi.ma>
2021-10-12 20:53:51 +0100

    Adds database connection

--- main/main.go
+++ main/main.go
@@ -3,8 +3,12 @@ package main
 
 import (
 	"blog/handlers"
+	"os"
 
+	"github.com/fatih/color"
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
@@ -12,6 +16,15 @@ func main() {
 	var app *fiber.App = fiber.New(fiber.Config{
 		CaseSensitive: true,
 	})
+
+	// Creates database if it does not exist and opens a connection with it
+	_, err := gorm.Open(sqlite.Open("database.db"), &gorm.Config{})
+	if err != nil {
+		color.New(color.BgRed).Add(color.FgWhite).Add(color.Bold).Println(" Failed to connect to database ")
+		os.Exit(1)
+	} else {
+		color.New(color.BgGreen).Add(color.FgWhite).Add(color.Bold).Println(" Connect to database successfuly ")
+	}
 
 	// Creates a handler struct for posts
 	var ph handlers.PostsHandler = handlers.NewPostsHandler()
