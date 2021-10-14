23e82398fa76b7e0be6c1d3000f8e51b44c642fd
Med Imrane <imrane@chehabi.ma>
2021-10-14 17:42:18 +0100

    Changed the name of Post struct in JSON conversion

--- data/post.go
+++ data/post.go
@@ -7,10 +7,10 @@ import (
 )
 
 type Post struct {
-	ID        uint `gorm:"primarykey"`
-	Title     string
-	Content   string
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt gorm.DeletedAt `gorm:"index"`
+	ID        uint           `gorm:"primarykey" json:"id"`
+	Title     string         `json:"title"`
+	Content   string         `json:"content"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"-"`
 }
